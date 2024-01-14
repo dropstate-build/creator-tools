1dddf121659044b82ae9d082cac4cc0f7f17ea98
ijas.j <ijas.a@marketfeed.com>
2024-01-14 21:42:48 +0530

    develop init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:creator_tools/textsyles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -41,30 +42,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 120,
+        vertical: 62,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '1',
+              style: dmSerifRegular120,
             ),
+            Text('2'),
           ],
-        ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/textsyles.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+final pjsRegular16 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w400, fontSize: 16);
+final pjsRegular18 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w400, fontSize: 18);
+
+final pjsMedium14 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w500, fontSize: 14);
+final pjsMedium16 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w500, fontSize: 16);
+final pjsMedium20 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w500, fontSize: 20);
+final pjsMedium36 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w500, fontSize: 36);
+
+final pjsSemiBold12 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w600, fontSize: 12);
+final pjsSemiBold14 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w600, fontSize: 14);
+final pjsSemiBold16 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w600, fontSize: 16);
+
+final pjsBold14 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w700, fontSize: 14);
+final pjsBold16 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w700, fontSize: 16);
+final pjsBold18 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w700, fontSize: 18);
+final pjsBold20 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w700, fontSize: 20);
+final pjsBold40 = GoogleFonts.plusJakartaSans(fontWeight: FontWeight.w700, fontSize: 40);
+
+final dmSerifRegular32 = GoogleFonts.dmSerifDisplay(fontWeight: FontWeight.w400, fontSize: 32);
+final dmSerifRegular48 = GoogleFonts.dmSerifDisplay(fontWeight: FontWeight.w400, fontSize: 48);
+final dmSerifRegular76 = GoogleFonts.dmSerifDisplay(fontWeight: FontWeight.w400, fontSize: 76);
+final dmSerifRegular120 = GoogleFonts.dmSerifDisplay(fontWeight: FontWeight.w400, fontSize: 120);
